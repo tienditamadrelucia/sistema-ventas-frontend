16aef633492bbafffe1edbddb07b5c31a00db512
tienditamadrelucia <tienditamadrelucia@gmail.com>
2026-04-28 18:32:29 -0400

    20 toma inventario

--- src/pages/Inventario.js
+++ src/pages/Inventario.js
@@ -3,7 +3,8 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   obtenerInventario,
-  guardarInventario,
+  guardarTomaInventario,
+  editarTomaInventario,
   buscarInventarioGuardado,    
   eliminarTomaInventario, 
   crearEntrada, 
