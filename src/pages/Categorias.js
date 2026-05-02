822669fa33d93633fbf98a6edfd954663360f218
tienditamadrelucia <tienditamadrelucia@gmail.com>
2026-05-02 18:09:47 -0400

    varias validaciones 6

--- src/pages/Categorias.js
+++ src/pages/Categorias.js
@@ -192,6 +192,7 @@ const Categorias = () => {
   // -------------------------
 
   const eliminarCategoria = async (id) => {
+    console.log("id ", id);
   if (!window.confirm("¿Eliminar esta categoría?")) return;
   try {
     const res = await fetch(`${API_URL}/api/categorias/${id}`, {
