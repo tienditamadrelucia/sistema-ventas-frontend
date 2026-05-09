f95706b2814321f766092f4c43bf19129e50db06
tienditamadrelucia <tienditamadrelucia@gmail.com>
2026-05-09 18:19:07 -0400

    ventas varios 15

--- src/pages/Ventas.js
+++ src/pages/Ventas.js
@@ -1653,7 +1653,7 @@ const cargarFacturaParaPago = async (venta) => {
             Crédito
           </button>
         </div>
-        <div style={{width: "915", display: "flex", justifyContent:"flex-end"}}>
+        <div style={{width: "515", display: "flex", justifyContent:"left"}}>
           <button
             onClick={() => {
               registrarAccion("Guardó factura sin pago");
