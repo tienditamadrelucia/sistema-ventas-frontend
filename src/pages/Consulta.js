4ccb06bdeaaad0b8bedd2c252505c4af40f4f93b
tienditamadrelucia <tienditamadrelucia@gmail.com>
2026-05-13 21:23:54 -0400

    credito 16

--- src/pages/Consulta.js
+++ src/pages/Consulta.js
@@ -108,8 +108,8 @@ const Consulta = () => {
   const cargarTasaDeLaFactura = async (fechaFactura) => {
     try {
       //const fechaFactura = venta.fecha.substring(0, 10);
-      //const fecha = fechaFactura.substring(0, 10);
-      const res = await fetch(`${API_URL}/api/tasas/por-fecha/${fechaFactura}`);
+      const fecha = fechaFactura.substring(0, 10);
+      const res = await fetch(`${API_URL}/api/tasas/por-fecha/${fecha}`);
       const data = await res.json();
       if (data.ok) {
         setTasaDolar(data.tasa.tasaDolar);
