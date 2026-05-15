56c5c138961bf0bd55b1ce2b46d3623b165df05c
tienditamadrelucia <tienditamadrelucia@gmail.com>
2026-05-15 15:21:26 -0400

    movimientos 3

--- src/services/ser_movi.js
+++ src/services/ser_movi.js
@@ -4,20 +4,14 @@ import { API_URL } from "../config"; // ajusta la ruta según tu carpeta
 export async function Consultar(productoId, fechaInicio, fechaFin) {
   try {    
     const url = `${API_URL}/api/movimientos/${productoId}?fechaInicio=${fechaInicio}&fechaFin=${fechaFin}`;
-
-    const res = await fetch(url);
-    
+    const res = await fetch(url);   
     if (!res.ok) {
       throw new Error(`Error HTTP: ${res.status}`);
-    }
-        
+    }     
     //alert("RESPUESTA DEL SERVIDOR:\n" + texto);
-
     const data = await res.json();
-
     // El backend devuelve movimientosArray, no "movimientos"
-    return data.movimientosArray || [];
-
+    return data.movimientos || [];
   } catch (error) {
     alert("Error consultando movimientos services: " + error.message);
     return [];
